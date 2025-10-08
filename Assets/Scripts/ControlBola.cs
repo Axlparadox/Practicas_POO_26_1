0ae9f7ee8cbe0d43d16ea38b546f978274c05d66
Axlparadox <elitespase10@gmail.com>
2025-10-07 21:57:06 -0600

    Añado trabajo con camara de seguimiento

---
+++ Assets/Scripts/ControlBola.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class ControlBola : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    //Variables para apuntar
+    public float velocidadDeApuntado = 5f;
+    public float limiteIzquierdo = -2f;
+    public float limiteDerecho = 2f;
+
+    public float fuerzaDeLanzamiento = 1000f;
+
+    private bool hasSidoLanzada = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       //Expresion:mientras que haSidoLanzada sea falso puedes disparar
+        if (hasSidoLanzada==false)
+        {
+            Apuntar();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Lanzar();
+            }
+        }
+    }
+
+    void Apuntar()
+    {
+        //1.- Leer un input Horizontal de tipo Axis, te permite registrar entradas con las teclas A y D, Flecha Izquierda y Flecha Derecha
+        float inputHorizontal = Input.GetAxis("Horizontal");
+    
+        //2.- Mover la bola hacia los lados
+        transform.Translate(Vector3.right * inputHorizontal * velocidadDeApuntado * Time.deltaTime);
+
+        //3.- Delimitar el movimiento de la bola
+        Vector3 posicionActual = transform.position;
+        // transform.position me permite saber cual es la posicion actual de la bola en la escena
+        posicionActual.x = Mathf.Clamp(posicionActual.x, limiteIzquierdo, limiteDerecho);
+
+        transform.position = posicionActual;
+    }
+
+    void Lanzar()
+    {
+        hasSidoLanzada = true;
+        rb.AddForce(Vector3.forward * fuerzaDeLanzamiento);
+    }
+
+}//Bienvenido a la entrada al infierno
